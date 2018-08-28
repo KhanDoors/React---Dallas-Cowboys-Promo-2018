34bff2e4f435438e0be48e5bfdac0fc37eb5da28
unknown <syyadkhan@yahoo.com>
2018-08-28 19:52:30 -0400

    percentage animation

---
+++ dallascowboys2018/src/components/Highlights/Description.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>The Rundown</h2>
+        <div className="highlight_description">
+          Expecting to see the Cowboys open the season against the rival New
+          York Giants? Well, you weren't alone. Instead, they'll look to head
+          east to Charlotte and take on the Carolina Panthers, a playoff team
+          that consistently seems one step away from taking home the franchise's
+          first Lombardi trophy. Cam Newton is one of the best quarterbacks in
+          the NFL when he's firing on all cylinders, and arguably one of the
+          worst when he's off his game. Making him more apt to the former is
+          second-year star Christian McCaffrey, a dynamo who can hit a home run
+          with every touch of the ball, and perennial All-Pro linebacker Luke
+          Kuechly is healthy and ready to again anchor a defense that still
+          boasts a time-defying Julius Peppers. The Cowboys will have their
+          hands full in this contest, but Newton can be forced into poor throws
+          if pressure is applied while being able to contain him in the pocket.
+          DeMarcus Lawrence can most certainly get the job done. The Cowboys
+          will drop the hammer that is Ezekiel Elliott, and win this one big for
+          Syyad and the boys over at Lake Mary.
+        </div>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
---
+++ dallascowboys2018/src/components/Highlights/Discount.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
+
+class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 99
+  };
+
+  porcentage = () => {
+    if (this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      });
+    }
+  };
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.porcentage();
+    }, 20);
+  }
+
+  render() {
+    return (
+      <div className="center_wrapper">
+        <div className="discount_wrapper">
+          <Fade onReveal={() => this.porcentage()}>
+            <div className="discount_porcentage">
+              <span>{this.state.discountStart}%</span>
+              <span>Win</span>
+            </div>
+          </Fade>
+
+          <Slide right>
+            <div className="discount_description">
+              <h3>Chance of Winning</h3>
+              <p>
+                I think this team is going to shock a lot of people this year. I
+                think they are going to come out of the gate firing on opponents
+                and begin a great run to the playoffs.
+              </p>
+              <div>button</div>
+            </div>
+          </Slide>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Discount;
